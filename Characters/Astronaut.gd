79ee9c6fd5229c8dc276a81dcd620e63e1b788a2
ZvonimirKucis <zvonimirkucis@gmail.com>
2020-06-06 14:08:49 +0200

    made wall tileset cell instead of wall object

--- Characters/Astronaut.gd
+++ Characters/Astronaut.gd
@@ -1,12 +1,14 @@
 extends Character
 class_name Astronaut
 
-var inputs = {"right": Vector2.RIGHT,
-			"left": Vector2.LEFT,
-			"up": Vector2.UP,
-			"down": Vector2.DOWN}
+var inputs : Dictionary = {
+	"right": Vector2.RIGHT,
+	"left": Vector2.LEFT,
+	"up": Vector2.UP,
+	"down": Vector2.DOWN
+}
 
-func _unhandled_input(event):
+func _unhandled_input(event : InputEvent) -> void:
 		if movement_tween.is_active():
 			return
 		for dir in inputs.keys():
